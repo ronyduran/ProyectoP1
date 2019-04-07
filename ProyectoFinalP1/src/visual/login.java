1dd58089ee4c84c239adc67114678211dcbfa625
CBalbuena <CBalbuena@LPOC2130108.intra.edenorte.com.do>
2019-04-07 17:38:16 -0400

    Pru

--- ProyectoFinalP1/src/visual/login.java
+++ ProyectoFinalP1/src/visual/login.java
@@ -79,7 +79,7 @@ public class login extends JFrame {
 	public login() {
 		setTitle("Login del sistema");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 364, 340);
+		setBounds(100, 100, 324, 333);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
