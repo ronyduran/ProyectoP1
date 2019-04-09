0672ee633fff835fffe674c3fafd8bb85c8a60b6
karly rodriguez <karly rodriguez@LAPTOP-GHCGPULH>
2019-04-08 23:23:25 -0400

    Actualizacion de inicio

--- ProyectoFinalP1/src/visual/PantallaPrincipal.java
+++ ProyectoFinalP1/src/visual/PantallaPrincipal.java
@@ -42,7 +42,7 @@ public class PantallaPrincipal extends JFrame implements Runnable  {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
